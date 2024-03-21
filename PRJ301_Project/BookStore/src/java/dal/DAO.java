8b058157086351f6edd61b941b597a8d0936a627
HuyTran2810 <140027999+HuyTran2810@users.noreply.github.com>
2024-03-21 13:25:51 +0700

    Add files via upload

---
+++ PRJ301_Project/BookStore/src/java/dal/DAO.java
@@ -0,0 +1,322 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import model.Account;
+import model.Category;
+import model.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author trinh
+ */
+public class DAO extends DBContext{
+
+    public List<Product> getAllProduct() {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getTop3() {
+        List<Product> list = new ArrayList<>();
+        String query = "select top 3 * from product";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getNext3Product(int amount) {
+        List<Product> list = new ArrayList<>();
+        String query = "SELECT *\n"
+                + "  FROM product\n"
+                + " ORDER BY id\n"
+                + "OFFSET ? ROWS\n"
+                + " FETCH NEXT 3 ROWS ONLY";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            ps.setInt(1, amount);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getProductByCID(String cid) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where cateID = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, cid);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getProductBySellID(int id) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where sell_ID = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> searchByName(String txtSearch) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where [name] like ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            
+            ps.setString(1, "%" + txtSearch + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Product getProductByID(String id) {
+        String query = "select * from product\n"
+                + "where id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        String query = "select * from Category";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(rs.getInt(1),
+                        rs.getString(2)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Product getLast() {
+        String query = "select top 1 * from product\n"
+                + "order by id desc";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public Account login(String user, String pass) {
+        String query = "select * from account\n"
+                + "where [user] = ?\n"
+                + "and pass = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public Account checkAccountExist(String user) {
+        String query = "select * from account\n"
+                + "where [user] = ?\n";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, user);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public void singup(String user, String pass) {
+        String query = "insert into account\n"
+                + "values(?,?,0,0)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ResultSet rs = ps.executeQuery();
+        } catch (Exception e) {
+        }
+    }
+
+    public void deleteProduct(String pid) {
+        String query = "delete from product\n"
+                + "where id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, pid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void insertProduct(String name, String image, String price,
+            String title, String description, String category, int sid) {
+        String query = "INSERT [dbo].[product] \n"
+                + "([name], [image], [price], [title], [description], [cateID], [sell_ID])\n"
+                + "VALUES(?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, image);
+            ps.setString(3, price);
+            ps.setString(4, title);
+            ps.setString(5, description);
+            ps.setString(6, category);
+            ps.setInt(7, sid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void editProduct(String name, String image, String price,
+            String title, String description, String category, String pid) {
+        String query = "update product\n"
+                + "set [name] = ?,\n"
+                + "[image] = ?,\n"
+                + "price = ?,\n"
+                + "title = ?,\n"
+                + "[description] = ?,\n"
+                + "cateID = ?\n"
+                + "where id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, image);
+            ps.setString(3, price);
+            ps.setString(4, title);
+            ps.setString(5, description);
+            ps.setString(6, category);
+            ps.setString(7, pid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+        List<Product> list = dao.getAllProduct();
+        List<Category> listC = dao.getAllCategory();
+
+        for (Category o : listC) {
+            System.out.println(o);
+        }
+    }
+
+}
